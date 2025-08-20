e42db93d3ceffdda81fdd592f32ae0f2479b9f03
MrCrashocaster <56044868+MrCrashocaster@users.noreply.github.com>
2025-08-20 14:18:29 -0500

    Create submit.js

---
+++ submit.js
@@ -0,0 +1,7 @@
+document.getElementById("id_of_textbox")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("id_of_button").click();
+    }
+});
